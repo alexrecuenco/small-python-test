a70105ec78d7db0de91bbf3ad3c09ae7cba6081b
Alex Recuenco <alex.recuenco@esave.es>
2023-11-30 14:55:39 +0100

    Fix linting

--- test_shortener.py
+++ test_shortener.py
@@ -4,6 +4,7 @@ from hypothesis import strategies as st
 
 from module import equals
 
+
 def test_empty_should_raise():
     with pytest.raises(TypeError):
         raise TypeError("test")
@@ -14,7 +15,10 @@ def test_url_encoding_start_with(url: str):
     assert equals(url, url)
 
 
-@given(url1=st.text(min_size=1), url2=st.text(min_size=1),)
-def test_generates_same(url1: str, url2:str):
+@given(
+    url1=st.text(min_size=1),
+    url2=st.text(min_size=1),
+)
+def test_generates_same(url1: str, url2: str):
     assume(url1 != url2)
     assert not equals(url1, url2)
